26ac26bf3791381b547cf55308638f88b31f0137
Micah Shute <micah.shute@gmail.com>
2018-07-04 23:48:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,10 @@ class Song
   def self.new_by_filename(file)
     data = file.split("/").last
     name = data.split(" - ")[1]
-    Song.new(name)
+    song = Song.new(name)
+    artist = Artist.new_by_filename(file)
+    song.artist = artist
+    song
   end
 
   attr_accessor :name, :artist
